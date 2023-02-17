8e5b1782d5769d8a7a54e9d2090f67bd64ae3724
Usuario <Usuario@DESKTOP-44RO4L3>
2023-02-17 15:45:09 -0600

    variables botones: btnIngresar, btnEntregar, btnPropietario, btnReparacion
    TextFields:
    Equipos: txtMarca, txtModelo, txtTipo, txtProblema
    Entrega: txtEquipo, txtNombreP, txtFechaE, txtDescripcionS
    Propietarios: txtApellidos, txtNombre, txtTelefono
    Reparacion: txtCodigo, txtDescripcion, txtTecnico, txtFecha
    
    
    jtable: 
    tblEntrega, tblEquipos, tblPropietarios, tblReparacion
    
    
    ACTUALIZACION: PCK MODELOS ESTA TERMINADO

--- src/pckModelos/propietarios.java
+++ src/pckModelos/propietarios.java
@@ -10,5 +10,39 @@ package pckModelos;
  * @author umg
  */
 public class propietarios {
+    String apellidosP;
+    String nombreP;
+    String telefono;
+    
+    public propietarios(String apellidosP, String nombreP, String telefono) {
+        this.apellidosP = apellidosP;
+        this.nombreP = nombreP;
+        this.telefono = telefono;
+        
+    }
+    public String getApellidosP() {
+        return apellidosP;
+    }
+
+    public void setApellidosP(String apellidosP) {
+        this.apellidosP = apellidosP;
+    }
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
     
 }
